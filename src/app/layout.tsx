c30d68984cd0b92ed0fdfc7c4cb25375a2e35f5e
Dominik Rubröder <dominik.rubroeder@icloud.com>
2023-06-15 15:16:19 +0200

    Start on building application ui, header, primary section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Logo from "@/app/components/Logo";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`grid gap-8 grid-rows-[max-content_max-content_1fr] mx-auto max-w-screen-md min-h-screen ${inter.className}`}>
+          <header className="flex justify-between gap-4 py-4 items-center">
+              <Logo />
+
+              <div>Other stuff</div>
+          </header>
+
+          <input type="text" placeholder="Search keyword..." className="text-xl font-semibold p-4 rounded-2xl bg-gray-100" value="keyboard"/>
+
+          {children}
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <h1>Hello world</h1>
+    <main className="">
+      <section className="flex gap-4 justify-between">
+          <div className="grid gap-2">
+              <h1 className="text-6xl font-bold">keyboard</h1>
+              <small className="text-purple-400">phonetic</small>
+          </div>
+          <button className="bg-purple-100 flex items-center justify-center p-4 rounded-full w-20 h-20 text-purple-600">P</button>
+      </section>
     </main>
   )
 }
